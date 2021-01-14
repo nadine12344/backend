8f18fe660cecf5cc4cb598bb513529806b83df88
nadine mohamed <nano_moh98@yahoo.com>
2021-01-14 02:12:56 +0200

    first2

--- server.js
+++ server.js
@@ -75,7 +75,7 @@ app.use('/',(req,res,next)=>{
  
 })
 
-app.use(cors())
+
 app.use(express.static(path.join(__dirname, "client", "build")))
 //app.UseCors(options => options.AllowAnyOrigin());
 app.use('/logging',log)
